b4d0cf6fbda7a5f267dc3b60cdc3305ef564c906
litt58 <lx520520>
2017-07-05 11:03:52 +0800

    初始化

---
+++ src/main/java/com/jzli/Application.java
@@ -0,0 +1,21 @@
+package com.jzli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * =======================================================
+ *
+ * @Company 产品技术部
+ * @Date ：2017/7/5
+ * @Author ：李金钊
+ * @Version ：0.0.1
+ * @Description ：
+ * ========================================================
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
